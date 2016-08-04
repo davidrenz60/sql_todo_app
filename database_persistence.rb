97fe82ddef07e4c45d72f3692d4aa378a09a8eaf
David Renz <davidrenz60@gmail.com>
2016-08-04 12:54:16 -0700

    finish database_persistence refactor

--- database_persistence.rb
+++ database_persistence.rb
@@ -7,13 +7,18 @@ class DatabasePersistence
   end
 
   def find_list(id)
-    sql = "SELECT * FROM lists WHERE id = $1"
+    sql = <<-SQL
+      SELECT lists.*,
+        COUNT(todos.id) AS todos_count,
+        COUNT(NULLIF(completed, true)) AS todos_remaining_count
+        FROM lists
+        LEFT JOIN todos ON lists.id = todos.list_id
+        WHERE lists.id = $1
+        GROUP BY lists.id
+        ORDER BY lists.name;
+    SQL
     result = query(sql, id)
-
-    tuple = result.first
-    list_id = tuple["id"].to_i
-    todos = find_todos_for_list(list_id)
-    { id: list_id, name: tuple["name"], todos: todos }
+    tuple_to_list_hash(result.first)
   end
 
   def all_lists
@@ -28,12 +33,7 @@ class DatabasePersistence
     SQL
 
     result = query(sql)
-    result.map do |tuple|
-      { id: tuple["id"].to_i,
-        name: tuple["name"],
-        todos_count: tuple["todos_count"].to_i,
-        todos_remaining_count: tuple["todos_remaining_count"].to_i }
-    end
+    result.map { |tuple| tuple_to_list_hash(tuple) }
   end
 
   def create_new_list(list_name)
@@ -71,8 +71,6 @@ class DatabasePersistence
     query(sql, list_id)
   end
 
-  private
-
   def query(statement, *params)
     @logger.info "#{statement}: #{params}"
     @db.exec_params(statement, params)
@@ -87,5 +85,14 @@ class DatabasePersistence
         name: todo_tuple["name"],
         completed: todo_tuple["completed"] == "t" }
     end
+  end
+
+  private
+
+  def tuple_to_list_hash(tuple)
+    { id: tuple["id"].to_i,
+      name: tuple["name"],
+      todos_count: tuple["todos_count"].to_i,
+      todos_remaining_count: tuple["todos_remaining_count"].to_i }
   end
 end
